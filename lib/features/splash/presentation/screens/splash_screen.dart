a83e0987f9192f80aa645fd97db0bcee9cc4db1a
poovarasan <poovarasan@thikse.in>
2025-07-17 16:56:20 +0530

    FEAT: Splash screen Bg Changes

--- lib/features/splash/presentation/screens/splash_screen.dart
+++ lib/features/splash/presentation/screens/splash_screen.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:fuoday/commons/widgets/k_linear_gradient_bg.dart';
 import 'package:fuoday/commons/widgets/k_svg.dart';
 import 'package:fuoday/core/constants/app_assets_constants.dart';
 import 'package:fuoday/core/constants/app_route_constants.dart';
-import 'package:fuoday/core/themes/app_colors.dart';
 import 'package:go_router/go_router.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -28,14 +26,11 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: KLinearGradientBg(
-        gradientColor: AppColors.employeeGradientColor,
-        child: Center(
-          child: KSvg(
-            height: 100.h,
-            svgPath: AppAssetsConstants.splashLogo,
-            boxFit: BoxFit.cover,
-          ),
+      body: Center(
+        child: KSvg(
+          height: 100.h,
+          svgPath: AppAssetsConstants.splashLogo,
+          boxFit: BoxFit.cover,
         ),
       ),
     );
